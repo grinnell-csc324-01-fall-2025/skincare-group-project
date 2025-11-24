657b93dcb70f1b12fe10d7b5d601b9614d568e98
Alma <almastar405@gmail.com>
2025-11-23 18:44:58 -0600

    welcomePage

--- skincare-app/app/(tabs)/index.jsx
+++ skincare-app/app/(tabs)/index.jsx
@@ -1,10 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Alert, StyleSheet} from 'react-native'
+import React, {useState} from 'react'
 
 const app = () => {
+  const [image, setImage] = useState(null);
+
+  function getImage(event) {
+    //get image from input
+  }
+
   return (
     <View style = {styles.container}>
-      <Text style = {styles.text}>Skincare Project</Text>
+      <h1 style = {styles.text}>Welcome!</h1>
+      <h2 style = {styles.text}>
+        In this app you will be able to scan or upload pictures of your skin to receive personalized skincare
+        recomendations depending on you skin and acne type. You also have the option to check whether a mole is cancerous.
+      </h2>
+      <h2 style = {styles.text}>
+        To start please choose one of the options below:
+      </h2>
+
+      <input type='file' id='image-input' onChange={getImage} ></input>
+
+      <View style={styles.buttons}>
+        <Button 
+          title="scan">
+
+        </Button>
+        <Button 
+          title="upload">
+          onPress={() => document.getElementById('image-input').click()}
+        </Button>
+      </View>
+
     </View>
   )
 }
@@ -14,12 +41,20 @@ export default app
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
+    alignItems: 'center',
+
   },
   text: {
-    fontSize: 50,
     color: '#9dc1fa',
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  buttons: {
+    marginTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+
+    gap:80
   }
 })
--- skincare-app/app/_layout.tsx
+++ skincare-app/app/_layout.tsx
@@ -14,10 +14,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      {/* <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      </Stack> */}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
